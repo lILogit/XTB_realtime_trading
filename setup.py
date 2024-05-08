e9be23145999d4b35fb4e9330211551939fc0e41
JK <jiri.kouba@gmail.com>
2024-05-08 17:46:35 +0200

    OK

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='XTB_trading',
+    version='',
+    packages=['XTBApi', 'XTBApi.logs', 'XTBApi.tests'],
+    url='',
+    license='',
+    author='jirka',
+    author_email='',
+    description=''
+)
